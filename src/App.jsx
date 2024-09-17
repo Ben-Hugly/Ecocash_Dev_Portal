e153817d0d5bb0fa9f3f2dc4e11b61e40010a6c6
Bernard Towindo <bernardtowindo38@gmail.com>
2024-09-17 22:48:50 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <span className='text-red-300 font-extrabold  text-lg'>
+          Click on the Vite and React logos to learn more
+      </span>
+      
+    </div>
+  )
+}
+
+export default App
